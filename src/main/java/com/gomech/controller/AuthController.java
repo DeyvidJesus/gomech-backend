bcde3bd4bdfb82b4531c4e44c519f4c07e4cbe2f
Deyvid Gondim <Deyvidgondim10@gmail.com>
2025-06-20 20:38:24 -0300

    feat(auth): add user registration

--- src/main/java/com/gomech/controller/AuthController.java
+++ src/main/java/com/gomech/controller/AuthController.java
@@ -2,7 +2,11 @@ package com.gomech.controller;
 
 import com.gomech.dto.AuthenticationDTO;
 import com.gomech.dto.TokenDTO;
+import com.gomech.dto.RegisterDTO;
+import com.gomech.model.User;
 import com.gomech.service.AuthService;
+import com.gomech.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +20,18 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> login(@RequestBody AuthenticationDTO data) {
         String token = authService.login(data.getEmail(), data.getPassword());
         return ResponseEntity.ok(new TokenDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterDTO dto) {
+        User user = userService.createUser(dto.getEmail(), dto.getPassword(), dto.getRoleId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
