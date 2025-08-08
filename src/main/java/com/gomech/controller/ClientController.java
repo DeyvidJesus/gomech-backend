5ac357a9f936988d2d29ea9c428dde90f5f85ec5
DeyvidJesus <deyvid.gondim@econverse.com.br>
2025-08-08 19:42:59 -0300

    chore: formatting api routes

--- src/main/java/com/gomech/controller/ClientController.java
+++ src/main/java/com/gomech/controller/ClientController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clients")
+@RequestMapping("/clients")
 public class ClientController {
     @Autowired
     private ClientService service;
