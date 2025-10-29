48e0f228e027f3cacfb87c7e16b099ba2df448cc
DeyvidJesus <deyvidgondim10@gmail.com>
2025-10-29 10:02:15 -0300

    chore: merge done

--- src/main/java/com/gomech/dto/Ai/AiResponseDTO.java
+++ src/main/java/com/gomech/dto/Ai/AiResponseDTO.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> 16690f57f3ffa06c233aefd0539beaa93b02f143
 @Setter
 @Getter
 public class AiResponseDTO {
@@ -21,28 +18,16 @@ public class AiResponseDTO {
 
     @JsonProperty("threadId")
     private String threadId;
-<<<<<<< HEAD
 
     @JsonProperty("videos")
     private Object videos;
     
     public AiResponseDTO() {}
     
-    public AiResponseDTO(String answer, String chart, String threadId,  List<Object> videos) {
+    public AiResponseDTO(String answer, String chart, String threadId, List<Object> videos) {
         this.answer = answer;
         this.chart = chart;
         this.threadId = threadId;
         this.videos = videos;
     }
-=======
-    
-    public AiResponseDTO() {}
-    
-    public AiResponseDTO(String answer, String chart, String threadId) {
-        this.answer = answer;
-        this.chart = chart;
-        this.threadId = threadId;
-    }
-
->>>>>>> 16690f57f3ffa06c233aefd0539beaa93b02f143
 }
--- src/main/java/com/gomech/dto/Ai/ChatResponseDTO.java
+++ src/main/java/com/gomech/dto/Ai/ChatResponseDTO.java
@@ -1,6 +1,5 @@
 package com.gomech.dto.Ai;
 
-<<<<<<< HEAD
 public record ChatResponseDTO(
         String content,
         String status,
@@ -8,6 +7,3 @@ public record ChatResponseDTO(
         String chart,
         Object videos
 ) {}
-=======
-public record ChatResponseDTO(String content, String status, String thread_id, String chart) {}
->>>>>>> 16690f57f3ffa06c233aefd0539beaa93b02f143
